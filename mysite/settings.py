da2ff27ff5014e6442912a0ad9e3cde6cf140e24
HJR <hjryu1816@gmail.com>
2021-02-26 22:01:50 +0900

    'update'

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,11 +14,7 @@ from pathlib import Path
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
 
-#OR
 
-#Read secret key from a file
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '&+h3wpihnq13vy8g)8$)gwq(c(pri4jp*tg$2^2+nm-jbqkp3='
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
+
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = []
